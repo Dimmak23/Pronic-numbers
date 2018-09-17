f98a11a587a15e0cb119a9844bad1acb6d59824c
Dmytro Kovryzhenko <36036315+Dimmak23@users.noreply.github.com>
2018-09-17 15:48:56 +0300

    Update Pronum.cpp

--- Pronum.cpp
+++ Pronum.cpp
@@ -1,25 +1,3 @@
-/*
-Program for challenge Pronic numbers:
-
-Pronic number is a number which is the product
-of two consecutive integers, that is, n=x*(x+1). 
-Write a program which, for a given input n,
-detect if the number is pronic or not. 
-
-Some Examples: 
-6: True 
-42: True 
-132: True 
-2550: True 
-10100: True 
-4: False 
-66: False 
-133: False 
-1666: False 
-17289: False 
-
-*/
-
 #include <iostream>
 #include <cmath>
 
